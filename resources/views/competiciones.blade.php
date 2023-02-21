3b7ef5f3dd38113a9693d709dc71fc42e45534c1
JUAN.GARCIA <juan.garcia@softeca.es>
2023-02-21 13:19:04 +0100

    Create css blade

--- resources/views/competiciones.blade.php
+++ resources/views/competiciones.blade.php
@@ -1,6 +1,3 @@
-<?php
-{{ URL::asset('css/app.css'); }}
-?>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -9,7 +6,7 @@
 
         <title>Competiciones</title>
 
-        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     </head>
     <style>
         
@@ -36,18 +33,18 @@ h1{
   }
   th{
     padding: 20px 15px;
-    text-align: left;
-    font-weight: 500;
-    font-size: 12px;
+    text-align: center;
+    font-weight: 750;
+    font-size: 20px;
     color: #fff;
     text-transform: uppercase;
   }
   td{
     padding: 15px;
-    text-align: left;
+    text-align: center;
     vertical-align:middle;
     font-weight: 300;
-    font-size: 12px;
+    font-size: 16px;
     color: #fff;
     border-bottom: solid 1px rgba(255,255,255,0.1);
   }
@@ -67,18 +64,18 @@ h1{
   
   
   /* follow me template */
-  .made-with-love {
+  .made-by {
     margin-top: 40px;
     padding: 10px;
     clear: left;
     text-align: center;
     font-size: 16px;
     font-family: arial;
-    color: black;
+    color: white;
   }
-  .made-with-love i {
+  .made-by i {
     font-style: normal;
-    color: black;
+    color: white;
     font-size: 18px;
     position: relative;
     top: 2px;
@@ -130,8 +127,17 @@ h1{
 
 
 <!-- follow me template -->
-<div class="made-with-love">
+<div class="made-by">
   <b>Made by
     <i>@jjgn</i>
   </b>
 </div>
+
+<script>
+    import './bootstrap';
+// '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
+$(window).on("load resize ", function() {
+    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+    $('.tbl-header').css({'padding-right':scrollWidth});
+  }).resize();
+</script>
