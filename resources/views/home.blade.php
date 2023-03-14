4489082e12d211247a4336e716f0b6d336a64b58
JUAN.GARCIA <juan.garcia@softeca.es>
2023-03-14 08:25:14 +0100

    Create home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,44 +4,35 @@
         <section>
 
             <div class="d-flex justify-content-between align-items-end mb-3">
-                <h1 class="pb-1">Project Football</h1>
+                <h1 class="pb-1" style="background: white">Project Football</h1>
             </div>
 
             <div class="home-menu">
                 <a href="/competiciones" class="main-nav-link">
-                    <button class="button btn-primary btn-lg">
+                    <button class="button btn-primary btn-lg" style="width:200px;">
                         <span>Competiciones</span>
                     </button>
                 </a>
 
                 <a href="/paises" class="main-nav-link">
-                    <button class="button btn-primary btn-lg">
+                    <button class="button btn-primary btn-lg" style="width:200px;">
                         <span>Países</span>
                     </button>
-                </a>
-            </div>
 
-            <div calss="home-menu">
                 <a href="/equipos" class="main-nav-link">
-                    <button class="button btn-primary btn-lg">
+                    <button class="button btn-primary btn-lg" style="width:200px;">
                         <span>Equipos</span>
                     </button>
                 </a>
 
                 <a href="/jugadores" class="main-nav-link">
-                    <button class="button btn-primary btn-lg">
+                    <button class="button btn-primary btn-lg" style="width:200px;">
                         <span>Jugadores</span>
                     </button>
                 </a>
             </div>
-                
-            {{-- <div>
-                <img src="{{url('/images/ball-project-football.jpg')}}" alt="home-blue-ball" width="100%" height="100%">
-            </div> --}}
         </section>
     </body>
-
-
 
 <script src="{{ asset('js/app.js') }}" defer></script>
 
